9e9bd92ae01e231bbf238db3070a523034a71088
rodgersxy <rodgersny99@gmail.com>
2020-04-25 20:31:00 +0300

    Updates

---
+++ precache-manifest.165078aa85bc865979f3428447963ed1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cb3044a2e16f7a9f39791fb40e75b7",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "3bb7101a6c4e6dc23477",
+    "url": "/react-app/static/css/main.92760222.chunk.css"
+  },
+  {
+    "revision": "208a8257bd8b37fe2407",
+    "url": "/react-app/static/js/2.208bbb3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.208bbb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb7101a6c4e6dc23477",
+    "url": "/react-app/static/js/main.19b82bc9.chunk.js"
+  },
+  {
+    "revision": "5a0a4501d5aeb93bf967",
+    "url": "/react-app/static/js/runtime-main.46503327.js"
+  },
+  {
+    "revision": "027ad0351b857d65d0274095b1c4de39",
+    "url": "/react-app/static/media/ps.027ad035.jpeg"
+  }
+]);
